7fefcc2e86ba8d197a18c6a22fe00d3b9b8209bd
Sindunata 61 <sindunata61@gmail.com>
2024-01-13 05:39:05 +0700

    fourth

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -102,7 +102,7 @@ const Experience = () => {
               ease-in-out"
               onClick={() =>
                 window.open(
-                  'resume link', //paste the link to your resume here
+                  'https://drive.google.com/file/d/12vbN7nn2B9NK5u845-9UPaI1SdQ9gwoY/view?usp=sharing', //paste the link to your resume here
                   '_blank'
                 )
               }
