aaf5cb693a4f313f26dd8562b6c1d8264a9b8650
Sindunata 61 <sindunata61@gmail.com>
2024-11-06 23:09:18 +0700

    change resume -2

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -102,7 +102,7 @@ const Experience = () => {
               ease-in-out"
               onClick={() =>
                 window.open(
-                  'https://drive.google.com/file/d/1KTpUlfk3XPe4YTfPmMG7EZuV5QSMwz8C/view?usp=sharing', //paste the link to your resume here
+                  'https://drive.google.com/file/d/1l9IXW4dRHJzgRk6rNkPtPcUoMHWmyC0c/view?usp=sharing', //paste the link to your resume here
                   '_blank'
                 )
               }
